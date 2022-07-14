14b5820ff395662acfe6b4332f4a05e28f22daa5
Md.Khurshid jahan <khurshid.jahan@northsouth.edu>
2022-07-14 13:52:56 +0600

    hackerarnk
    hacker rank

---
+++ variables.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+ int a;
+ long int b;
+ char ch;
+ float d;
+ double e;
+ scanf("%d %ld %c %f %lf" ,&a,&b,&ch,&d,&e);
+  printf("%d\n%lld\n%c\n%f\n%lf",a,b,ch,d,e);
+return 0;
+}
